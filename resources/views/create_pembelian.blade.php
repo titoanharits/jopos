5dff106151c44625ddd7e11f7114ac0b63b7905a
titoanharits <38245796+titoanharits@users.noreply.github.com>
2019-11-27 03:16:19 +0700

    fix pembelian sekian

--- resources/views/create_pembelian.blade.php
+++ resources/views/create_pembelian.blade.php
@@ -181,7 +181,7 @@
     var xmlhttp = new XMLHttpRequest();
     var value = document.getElementById("barang").value;
     if (value != "") {
-      xmlhttp.open("GET", "/pembelian/barang/" + value, false);
+      xmlhttp.open("GET", "pembelian/barang/" + value, false);
       xmlhttp.send(null);
       document.getElementById("detail_barang").innerHTML = xmlhttp.responseText;
     } else {
