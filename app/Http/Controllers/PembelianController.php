4c1ad944dd58389de07e5f6a6b79c8f92b3040eb
titoanharits <38245796+titoanharits@users.noreply.github.com>
2019-11-26 22:48:39 +0700

    fix pembelian

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -126,8 +126,7 @@ class PembelianController extends Controller
 
   public function tambahBarang(Request $request)
   {
-    $barang = DB::table('barangs')
-    ->where('id_barang', $request->id_barang)
+    $barang = DB::Barang::where('id_barang', $request->id_barang)
     ->first();
     $satuan_satu = $barang->satuan_satu;
     $satuan_dua = $barang->satuan_dua;
