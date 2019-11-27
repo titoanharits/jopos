d36c12a0532ec46bf50740e8d25354e375d7384d
titoanharits <38245796+titoanharits@users.noreply.github.com>
2019-11-27 15:20:48 +0700

    aut

--- app/Http/Controllers/DetailPembelianController.php
+++ app/Http/Controllers/DetailPembelianController.php
@@ -16,11 +16,8 @@ class DetailPembelianController extends Controller
   * @return \Illuminate\Http\Response
   */
 
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
-  
+
+
   public function index()
   {
     $pembelian = Pembelian::with('Supplier')
--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -17,10 +17,6 @@ class PembelianController extends Controller
   *
   * @return \Illuminate\Http\Response
   */
-  public function __construct()
-  {
-      $this->middleware('auth');
-  }
 
   public function index()
   {
